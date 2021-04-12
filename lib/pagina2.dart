f49a80872805223243ceb51ebae93673156a94db
Junior Heleno Avila <fuinhkon_san@hotmail.com>
2021-04-12 20:22:02 -0300

    Create pagina2.dart

---
+++ lib/pagina2.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'pagina3.dart';
+import 'login.dart';
+
+class Pagina2 extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aprendendo o alfabeto',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: VideoDemo(),
+    );
+  }
+}
+
+class VideoDemo extends StatefulWidget {
+  VideoDemo() : super();
+
+  final String title = "Aprendendo o alfabeto";
+
+  @override
+  VideoDemoState createState() => VideoDemoState();
+}
+
+class VideoDemoState extends State<VideoDemo> {
+  //
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    /*_controller = VideoPlayerController.network(
+        'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4');*/
+    _controller = VideoPlayerController.asset('assets/videos/abecedario.mp4');
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text("Aprendendo o alfabeto"),
+        ),
+        body: Container(
+            child: ListView(children: <Widget>[
+          FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                if (_controller.value.isPlaying) {
+                  _controller.pause();
+                } else {
+                  _controller.play();
+                }
+              });
+            },
+            child: Icon(
+                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
+          ),
+          Divider(height: 20),
+          ElevatedButton(
+            child: Text("Proxima pagina"),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => ThirdPage()));
+            },
+          ),
+          Divider(height: 20),
+          ElevatedButton(
+            child: Text("Voltar tudo"),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => LoginPage()));
+            },
+          )
+        ])));
+  }
+}
